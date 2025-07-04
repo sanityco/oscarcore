328b0cdadceb5094ea71e9d9453f8cfac1b01f99
sanityware <dinlergurkan@gmail.com>
2025-07-04 22:13:32 +0300

    Delete plugins directory

--- plugins/hello_plugin.py
+++
@@ -1,5 +0,0 @@
-
-def register(ostp_agent):
-    async def hello_handler(data):
-        return {"status": "ok", "message": "Hello from plugin!"}
-    ostp_agent.register_handler("/hello", hello_handler)
